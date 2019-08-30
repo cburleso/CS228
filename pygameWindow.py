8d15ee23470eb74d4725a4e92ce4fa3ed125cf65
Connor Burleson <cburleso@uvm.edu>
2019-08-30 08:15:20 -0400

    Code changes through Step 43

---
+++ pygameWindow.py
@@ -0,0 +1,11 @@
+import pygame
+class PYGAME_WINDOW:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((300, 100))
+
+class Prepare:
+    pass
+
+class Reveal:
+    pass
