13dd69c6a1a5c00c56db06803407cf3f58329be6
Connor Burleson <cburleso@uvm.edu>
2019-08-31 15:52:42 -0400

    Update Del01.py
    Imported required modules for Leap Motion compatibility

--- Del01.py
+++ Del01.py
@@ -1,27 +1,31 @@
-from pygameWindow import PYGAME_WINDOW
-import random
-x = 400
-y = 400
+import sys
+sys.path.insert(0, '..')
+import Leap
 
-def Perturb_Circle_Position():
-    global x, y
-    fourSidedDieRoll = random.randint(1,4)
-    if fourSidedDieRoll == 1:
-        x -= 1
-    elif fourSidedDieRoll == 2:
-        x += 1
-    elif fourSidedDieRoll == 3:
-        y -= 1
-    else:
-        y += 1
-        
-pygameWindow = PYGAME_WINDOW()
-
-print(pygameWindow)
-while True:
-    pygameWindow.Prepare()
-    pygameWindow.Draw_Black_Circle(x, y)
-    Perturb_Circle_Position()
-    pygameWindow.Reveal()
+##from pygameWindow import PYGAME_WINDOW
+##import random
+##x = 400
+##y = 400
+##
+##def Perturb_Circle_Position():
+##    global x, y
+##    fourSidedDieRoll = random.randint(1,4)
+##    if fourSidedDieRoll == 1:
+##        x -= 1
+##    elif fourSidedDieRoll == 2:
+##        x += 1
+##    elif fourSidedDieRoll == 3:
+##        y -= 1
+##    else:
+##        y += 1
+##        
+##pygameWindow = PYGAME_WINDOW()
+##
+##print(pygameWindow)
+##while True:
+##    pygameWindow.Prepare()
+##    pygameWindow.Draw_Black_Circle(x, y)
+##    Perturb_Circle_Position()
+##    pygameWindow.Reveal()
 
 
